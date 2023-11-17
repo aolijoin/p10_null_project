ab807c6862ab11f0e89746106823303c527ca7e0
世界 <aolijoin@gmail.com>
2023-11-17 14:51:43 +0800

    init初始空项目

---
+++ fe/vue.config.js
@@ -0,0 +1,13 @@
+const {defineConfig} = require('@vue/cli-service')
+module.exports = {
+    devServer: {
+        port: 80,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080/api',
+                changeOrigin: true,
+                pathRewrite: {'^/api': "/"}
+            }
+        }
+    }
+}
---
+++ serve/src/main/java/cn/jiyun/Application.java
@@ -0,0 +1,16 @@
+package cn.jiyun;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("cn.jiyun.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("启动成功!!!");
+    }
+}
