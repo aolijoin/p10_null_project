4d7d0c28ffe8cca9fcbfde062fef4f540537950a
世界 <aolijoin@gmail.com>
2023-12-08 11:44:20 +0800

    购物车1.0

--- serve/src/main/java/cn/jiyun/service/CategoryService.java
+++ serve/src/main/java/cn/jiyun/service/CategoryService.java
@@ -2,7 +2,6 @@ package cn.jiyun.service;
 
 import cn.jiyun.pojo.Category;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.sun.xml.internal.ws.wsdl.writer.document.Service;
 
 public interface CategoryService extends IService<Category> {
 }
