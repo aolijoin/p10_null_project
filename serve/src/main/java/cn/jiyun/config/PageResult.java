665cd6ef40b5331e8abce08b7a2b2fbb6ded1f3b
世界 <aolijoin@gmail.com>
2023-12-08 10:53:12 +0800

    空项目

---
+++ serve/src/main/java/cn/jiyun/config/PageResult.java
@@ -0,0 +1,26 @@
+package cn.jiyun.config;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+public class PageResult<T> implements Serializable {
+    private Long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(Long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+}
